2e81eee30803d3632f6b3455ac626582bea6c1d6
unknown <alihaiderizvi.you@gmail.com>
2021-11-16 16:27:20 +0500

    Spinner

--- src/components/ui/spinner.js
+++ src/components/ui/spinner.js
@@ -1,11 +1,13 @@
 import React from 'react'
-
-const spinner = () => {
+import spinner from '../../img/spinner.gif'
+const Spinner = () => {
     return (
-        <div>
-            
-        </div>
+        <img
+            src={spinner}
+            style={{width:'200px', margin:'auto', display:'block'}}
+            alt='Loading...'
+        />
     )
 }
 
-export default spinner
+export default Spinner

5e6f2c835c5f737dc0411604daac91df5f9914cc
unknown <alihaiderizvi.you@gmail.com>
2021-11-16 15:52:45 +0500

    Fetching api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css'
+import Header from './components/ui/Header';
+import CharacterGrid from './components/characters/CharacterGrid';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  
+  useEffect(() => {
+    const fetchItems = async () => {
+      const res = await axios(`https://www.breakingbadapi.com/api/characters`)
+      setItems(res.data)
+      setIsLoading(false)
+    }
+    fetchItems()
+  }, [])
+
   return (
-    < >
-       
+    <> 
+      <Header /> 
+      <CharacterGrid items={items} isLoading={isLoading}/>
     </>
   );
 }
---
+++ src/components/ui/Header.js
@@ -0,0 +1,14 @@
+import logo from '../../img/logo.png'
+
+const Header = () => {
+    return (
+        <header className='container'>
+            <img
+                src={logo}
+                alt=''
+            />
+        </header>
+    )
+}
+
+export default Header
